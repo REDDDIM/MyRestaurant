4fa83933fe28b42eb211c2c27861aa20bbbca856
a.abdullin <airupaza@gmail.com>
2019-12-23 23:10:52 +0500

    fixes

--- controllers/src/main/java/config/RestResponseEntityExceptionHandler.java
+++ controllers/src/main/java/config/RestResponseEntityExceptionHandler.java
@@ -1,11 +1,14 @@
 package config;
 
 import dto.ErrorDto;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -13,55 +16,54 @@ import services.exceptions.OrderException;
 import services.exceptions.PositionException;
 import services.exceptions.UserException;
 
+import javax.persistence.NoResultException;
+
 @ControllerAdvice
-public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+public class RestResponseEntityExceptionHandler  {
 
     @ExceptionHandler(value = Exception.class)
-    protected ResponseEntity<Object> handleConflict(
+    protected ResponseEntity<ErrorDto> handleConflict(
             Exception ex, WebRequest request) {
         ErrorDto errorDto = new ErrorDto();
         errorDto.setErrorType(ex.getClass().toString());
         errorDto.setMessage("An error has occurred : "+ex.getMessage());
-        return handleExceptionInternal(ex, errorDto,
-                new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
-    }
-
-    @ExceptionHandler(NoHandlerFoundException.class)
-    public ResponseEntity<Object> handleError404(Exception ex, WebRequest request)   {
-        ErrorDto errorDto = new ErrorDto();
-        errorDto.setErrorType(ex.getClass().toString());
-        errorDto.setMessage("An error has occurred : "+ex.getMessage());
-        return handleExceptionInternal(ex, errorDto,
-                new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+        return new ResponseEntity(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(value = PositionException.class)
-    protected ResponseEntity<Object> handlePositionConflict(
+    protected ResponseEntity<ErrorDto> handlePositionConflict(
             Exception ex, WebRequest request) {
         ErrorDto errorDto = new ErrorDto();
         errorDto.setErrorType(ex.getClass().toString());
         errorDto.setMessage("An error has occurred : "+ex.getMessage());
-        return handleExceptionInternal(ex, errorDto,
-                new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+        return new ResponseEntity(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(value = OrderException.class)
-    protected ResponseEntity<Object> handleOrderConflict(
+    protected ResponseEntity<ErrorDto> handleOrderConflict(
             Exception ex, WebRequest request) {
         ErrorDto errorDto = new ErrorDto();
         errorDto.setErrorType(ex.getClass().toString());
         errorDto.setMessage("An error has occurred : "+ex.getMessage());
-        return handleExceptionInternal(ex, errorDto,
-                new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+        return new ResponseEntity(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(value = UserException.class)
-    protected ResponseEntity<Object> handleUserConflict(
+    protected ResponseEntity<ErrorDto> handleUserConflict(
             Exception ex, WebRequest request) {
         ErrorDto errorDto = new ErrorDto();
         errorDto.setErrorType(ex.getClass().toString());
         errorDto.setMessage("An error has occurred : "+ex.getMessage());
-        return handleExceptionInternal(ex, errorDto,
-                new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+        return new ResponseEntity(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = NoHandlerFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    protected ResponseEntity<ErrorDto> handle404Conflict(
+            Exception ex, WebRequest request) {
+        ErrorDto errorDto = new ErrorDto();
+        errorDto.setErrorType(ex.getClass().toString());
+        errorDto.setMessage("An error has occurred : "+ex.getMessage());
+        return new ResponseEntity(errorDto, HttpStatus.NOT_FOUND);
     }
 }
